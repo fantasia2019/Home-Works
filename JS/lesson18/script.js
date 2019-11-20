e8314b62ba64952e78b7933cd5927c1dc9ca740f
fourseason2019 <57719415+fourseason2019@users.noreply.github.com>
2019-11-20 10:40:28 +0200

    home works html/2 виправила/7-8/9/13 and JS/17/18

--- JS/lesson18/script.js
+++ JS/lesson18/script.js
@@ -10,32 +10,11 @@ if (a === '1'){
     console.log('Неверно');
 }
 
-// Проверка
-
-let р = 1;
-if (р === 1){
-    console.log('Верно');   // Верно
-} else {
-    console.log('Неверно');
-}
-
-let к = 3;
-if (к === 3){               // Верно
-    console.log('Верно');
-} else {
-    console.log('Неверно');
-}
-
 
 /* 2. Если переменная item не равна true, то выведите "Верно", иначе выведите "Неверно";.
 Проверьте работу скрипта при item, равном true, false. Напишите два варианта скрипта - с короткой записью
 и с длинной.
 */
-
-console.log( 'item' != true ); //true
-console.log( 'item' == true ); //false
-console.log( 'item' == false ); //false
-
 
 let item = true;
 if (item != true){
@@ -53,67 +32,37 @@ let result = (item != true )? 'Верно' : 'Неверно';
 Проверьте работу скрипта при b, равном 4, 0, -3, 2.
 */
 
-let b = prompt ('Введите число');
-if (b >= 4 ){
-    console.log('Неверно');    
-}else if(b <= 0 ){
-    console.log('Неверно');    
-}else{
+let b;
+if ( b > 0 || b < 4 ) {
     console.log('Верно');
+}else{
+    console.log('Неверно');
 }
 
-/* Такий варіант можна використовувати ?????????????????????????????????????
+// другий вариант
 
-let b = prompt ('Введите число');
-if ( 0 < b < 4 ){
+let e = prompt ('Введите число');
+if ( 0 < e < 4 ){
     console.log('Верно');    
 }else{
     console.log('Неверно');    
 }
+
+
+
+/* 4. Если переменная w больше 3-х и меньше 12-ти, или переменная q больше или равна 7-
+и и меньше 15-ти, то выведите 'Верно', в противном случае выведите "Неверно".
 */
 
-// Проверка
-
-let c=4;
-if (c >= 4 ){                    // Неверно
-    console.log('Неверно');    
-}else if(c <= 0 ){
-    console.log('Неверно');    
+let w;
+let q;
+if ( w > 3 || b < 12 )  {
+    console.log('Верно');
+}else if( q >= 7 || b < 15 ){
+    console.log('Верно');
 }else{
-    console.log('Верно');
+    console.log('Неверно');
 }
-
-
-let d=0;
-if (d >= 4 ){
-    console.log('Неверно');    
-}else if(d <= 0 ){              // Неверно
-    console.log('Неверно');    
-}else{
-    console.log('Верно');
-}
-
-
-
-let e=-3;
-if (e >= 4 ){
-    console.log('Неверно');    
-}else if(e <= 0 ){              // Неверно 
-    console.log('Неверно');     
-}else{
-    console.log('Верно');
-}
-
-
-let m=2;
-if (m >= 4 ){
-    console.log('Неверно');    
-}else if(m <= 0 ){
-    console.log('Неверно');    
-}else{                          // Верно
-    console.log('Верно');
-}
-
 
 /* 4. Если переменная l больше 3-х и меньше 12-ти, или переменная v больше или равна 7-
 и и меньше 15-ти, то выведите 'Верно', в противном случае выведите "Неверно".
@@ -143,26 +92,6 @@ if (2 < month < 6 ){
     console.log('Winter');
 }
 
-
-// намагалася зробити через ++, нічого не вийшло. Чи такий запис взагалі недопустимий????????????????
-
-/*
-for (let a=0; a<12; a++){
-
-if (2 < a < 6 ){
-    console.log('Spring');    
-}else if(5 < a < 9 ){
-    console.log('Summer');   
-}else if(8 < a <12 ){
-    console.log('Autumn');     
-}else{ 
-    console.log('Winter');
-}
-} 
-
-/*
-
-
 /*6. Выведите в консоли столбец четных чисел в промежутке от 0 до 100. (используя for или while)
 */
 
@@ -178,8 +107,8 @@ let number = 0;
 while( number < 100){
     if (number % 2 == 0) {
     console.log(number); 
-    number++;
     }
+    number++;
 }
 
 
