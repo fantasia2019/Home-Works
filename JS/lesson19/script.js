ed7c47aecd4c6691e3c8c7a9536490ae47814a26
fourseason2019 <57719415+fourseason2019@users.noreply.github.com>
2019-11-23 02:12:28 +0200

    home work /js/ 17/18/19/20

--- JS/lesson19/script.js
+++ JS/lesson19/script.js
@@ -1,2 +1,71 @@
+'use strict';
 
+/* 1. Сделайте функцию isNumberInRange, которая параметром принимает число и проверяет, что оно 
+больше нуля и меньше 10. Если это так - пусть функция возвращает true, если не так - false.*/
+
+function isNumberInRange(x){
+    if (x > 0 && x < 10){
+        console.log(true);
+    }else{
+        console.log(false);
+    }
+}
+
+/*2. Сделайте функцию isEven() (even - это четный), которая параметром принимает целое число 
+и проверяет: четное оно или нет. Если четное - пусть функция возвращает true, если нечетное - false.*/
+
+function isEven(n){
+    for (let b = 2; b < n; b++ ){
+if ( b % 2 == 0 ) return true;
+    }
+    return false;
+}
+
+
+
+/*3. Переписать код из конструкции if в switch
+
+var value = +prompt('value?', '');
+if (value == 0) {
+  alert( 0 );
+}
+if (value == 1) {
+  alert( 1 );
+}
+if (value == 2 || value == 3) {
+  alert( '2,3' );
+}
+
+*/
+
+let value = +prompt;
+switch(value){
+  case 0:
+    alert(0);
+    break;
+  case 1:
+    alert(1);
+    break;
+  case 2:
+  case 3:
+    alert('2,3');
+  break;
+}
+
+
+/*  4. Напишите функцию min(a,b), которая возвращает меньшее из чисел a,b 
+
+Пример вызовов: 
+min(3, 5) // вернет 3
+min(5, -1) // вернет -1 
+
+*/
+
+function min(a, b){
+  if(a < b ) {
+    return a;
+  } else {
+    return b;
+  }
+}
 
